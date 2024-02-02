142d06bcba48767ceeb1e5042eb51515933b21ae
Denis Klymenko <dklymenko@blackbird-lab.com>
2024-02-02 21:44:36 +0100

    update import

--- src/shared/UI/index.ts
+++ src/shared/UI/index.ts
@@ -1,7 +1,6 @@
 export * from './Form';
 export * from './Title';
 export * from './Buttons';
-export * from './WebVitals';
 export * from './BasicContainer';
 export * from './BasicSvgIcon';
 export * from './BasicCollapse';
